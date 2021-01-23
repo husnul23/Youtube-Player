b7cd08d216623fb14514fdd239f0db1ef62f56c7
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2021-01-23 20:48:08 +0700

    add on click listener

--- app/src/main/java/com/husnul23/youtubeplayer/MainActivity.kt
+++ app/src/main/java/com/husnul23/youtubeplayer/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.husnul23.youtubeplayer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.IllegalArgumentException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnPlaySingle.setOnClickListener(this)
+        btnStandalone.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        val intent = when (view.id) {
+            R.id.btnPlaySingle -> Intent(this, YoutubeActivity::class.java)
+            R.id.btnStandalone -> Intent(this, StandAloneActivity::class.java)
+            else -> throw IllegalArgumentException("Undefined button clicked")
+        }
+        startActivity(intent)
     }
 }
