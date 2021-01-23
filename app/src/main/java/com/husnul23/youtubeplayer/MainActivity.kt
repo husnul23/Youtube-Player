8546d748789560a7c83fcfa094a2a29d58d71c27
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2021-01-23 21:37:23 +0700

    add error handling if youtube was disabled

--- app/src/main/java/com/husnul23/youtubeplayer/MainActivity.kt
+++ app/src/main/java/com/husnul23/youtubeplayer/MainActivity.kt
@@ -8,7 +8,6 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.IllegalArgumentException
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,7 +19,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(view: View) {
         val intent = when (view.id) {
             R.id.btnPlaySingle -> Intent(this, YoutubeActivity::class.java)
-            R.id.btnStandalone -> Intent(this, StandAloneActivity::class.java)
+            R.id.btnStandalone -> Intent(this, StandaloneActivity::class.java)
             else -> throw IllegalArgumentException("Undefined button clicked")
         }
         startActivity(intent)
--- app/src/main/java/com/husnul23/youtubeplayer/StandAloneActivity.kt
+++ app/src/main/java/com/husnul23/youtubeplayer/StandAloneActivity.kt
@@ -8,36 +8,39 @@ import com.google.android.youtube.player.YouTubeStandalonePlayer
 import kotlinx.android.synthetic.main.activity_standalone.*
 import java.lang.IllegalArgumentException
 
-class StandAloneActivity: AppCompatActivity(), View.OnClickListener {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+class StandaloneActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_standalone)
 
         btnPlayVideo.setOnClickListener(this)
         btnPlayList.setOnClickListener(this)
 
-//        btnPlayVideo.setOnClickListener(object : View.OnClickListener {
+//        btnPlayVideo.setOnClickListener(object :  View.OnClickListener {
 //            override fun onClick(p0: View?) {
-//
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 //            }
+//        })
+//        btnPlayVideo.setOnClickListener(View.OnClickListener { v ->
+//            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 //        })
 
 //        val listener = View.OnClickListener { v ->
+//            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 //        }
 //        btnPlayVideo.setOnClickListener(listener)
-//        btnPlayList.setOnClickListener(listener)
-
+//        btnPlaylist.setOnClickListener(listener)
     }
 
     override fun onClick(view: View) {
         val intent = when (view.id) {
             R.id.btnPlayVideo -> YouTubeStandalonePlayer.createVideoIntent(
-                    this, getString(R.string.GOOGLE_API_KEY), YOUTUBE_VIDEO_ID)
+                    this, getString(R.string.GOOGLE_API_KEY), YOUTUBE_VIDEO_ID, 0, true, false)
             R.id.btnPlayList -> YouTubeStandalonePlayer.createPlaylistIntent(
-                    this, getString(R.string.GOOGLE_API_KEY), YOUTUBE_PLAYLIST)
+                    this, getString(R.string.GOOGLE_API_KEY), YOUTUBE_PLAYLIST, 0, 0, true, true)
             else -> throw IllegalArgumentException("Undefined button clicked")
         }
         startActivity(intent)
     }
-
 }
